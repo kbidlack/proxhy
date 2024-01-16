70cf32917fa30487cc0e88d00198150883bc16f3
Kavi Bidlack <ksbidlack@gmail.com>
2024-01-15 16:17:47 -0800

    changed some things ugh i'm too lazy to write a commit message for this

--- proxhy/auth/__init__.py
+++ proxhy/auth/__init__.py
@@ -20,7 +20,7 @@ async def load_auth_info() -> tuple[str]:
         with open(auth_cache_path, "rb") as file:
             auth_data = file.read()
     else:
-        auth_data = base64.b64encode(str(tuple("" for _ in range(7))).encode("utf-8"))
+        auth_data = base64.b64encode(str(tuple("" for _ in range(6))).encode("utf-8"))
 
     (
         email,
@@ -29,13 +29,11 @@ async def load_auth_info() -> tuple[str]:
         uuid,
         access_token,
         access_token_gen_time,
-        api_key,
     ) = ast.literal_eval(base64.b64decode(auth_data).decode("utf-8"))
 
     if not (email and password):
         email = input("Enter your Microsoft login email: ").strip()
         password = getpass.getpass("Enter your Microsoft login password: ").strip()
-        api_key = input("Enter your Hypixel API Key: ")
 
         print("Generating credentials...", end="", flush=True)
         user_profile = await MsMcAuth().login(email, password)
@@ -60,10 +58,9 @@ async def load_auth_info() -> tuple[str]:
                         user_profile.uuid,
                         user_profile.access_token,
                         access_token_gen_time,
-                        api_key,
                     )
                 ).encode("utf-8")
             )
         )
 
-    return user_profile.access_token, user_profile.username, user_profile.uuid, api_key
+    return user_profile.access_token, user_profile.username, user_profile.uuid
