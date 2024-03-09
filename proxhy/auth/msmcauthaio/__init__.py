399da545c2b304cfd6a76d9e1367a7129b869643
Kavi Bidlack <ksbidlack@gmail.com>
2024-03-08 22:29:20 -0800

    more changes

--- proxhy/auth/msmcauthaio/__init__.py
+++ proxhy/auth/msmcauthaio/__init__.py
@@ -1,23 +1,21 @@
-from .helpers import Microsoft, Xbox
 from .client import MsMcAuth
-from .http import Http
-
-from .models import (
-    XSTSAuthenticateResponse,
-    XblAuthenticateResponse,
-    UserLoginResponse,
-    PreAuthResponse,
-    UserProfile
-)
-
 from .errors import (
-    XstsAuthenticationFailed,
-    XblAuthenticationFailed,
-    LoginWithXboxFailed,
-    InvalidCredentials,
-    MsMcAuthException,
-    TwoFactorAccount,
-    NoXboxAccount,
     ChildAccount,
-    NotPremium
+    InvalidCredentials,
+    LoginWithXboxFailed,
+    MsMcAuthException,
+    NotPremium,
+    NoXboxAccount,
+    TwoFactorAccount,
+    XblAuthenticationFailed,
+    XstsAuthenticationFailed,
+)
+from .helpers import Microsoft, Xbox
+from .http import Http
+from .models import (
+    PreAuthResponse,
+    UserLoginResponse,
+    UserProfile,
+    XblAuthenticateResponse,
+    XSTSAuthenticateResponse,
 )
