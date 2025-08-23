c07556bf4ecff0dc3065afd86a3c6efc6023db05
Kavi Bidlack <ksbidlack@gmail.com>
2025-08-23 13:28:45 -0700

    improve events.py type hinting

--- core/events.py
+++ core/events.py
@@ -1,9 +1,20 @@
 from dataclasses import dataclass
-from typing import Any, Awaitable, Callable, Literal
+from typing import TYPE_CHECKING, Any, Awaitable, Callable, Literal, TypeVar
 
 from protocol.datatypes import Buffer
 
 from .net import State
+
+if TYPE_CHECKING:
+    from .plugin import Plugin
+    from .proxy import Proxy
+
+T = TypeVar("T", bound="Plugin | Proxy")
+
+type ListenerFunction[T] = Callable[[T, Buffer], Awaitable[Any]]
+type DecoratorType[T] = Callable[[ListenerFunction[T]], ListenerFunction[T]]
+type EventListenerFunction = Callable[[Any, Any], Awaitable[Any]]
+type EventDecoratorType = Callable[[EventListenerFunction], EventListenerFunction]
 
 
 @dataclass
@@ -20,7 +31,7 @@ def listen_client(
     state: State = State.PLAY,
     blocking=False,
     override=False,
-):
+) -> DecoratorType[T]:
     return _listen("client", packet_id, state, blocking, override)
 
 
@@ -29,7 +40,7 @@ def listen_server(
     state: State = State.PLAY,
     blocking=False,
     override=False,
-):
+) -> DecoratorType[T]:
     return _listen("server", packet_id, state, blocking, override)
 
 
@@ -39,8 +50,8 @@ def _listen(
     state: State = State.PLAY,
     blocking=False,
     override=False,
-):
-    def wrapper(func: Callable[[Any, Buffer], Awaitable[Any]]):
+) -> DecoratorType[T]:
+    def wrapper(func: ListenerFunction[T]) -> ListenerFunction[T]:
         listener_meta = PacketListener(source, packet_id, state, blocking, override)
         setattr(func, "_listener_meta", listener_meta)
 
@@ -49,8 +60,8 @@ def _listen(
     return wrapper
 
 
-def subscribe(event: str):
-    def wrapper(func: Callable[[Any, Any], Awaitable[Any]]):
+def subscribe(event: str) -> EventDecoratorType:
+    def wrapper(func: EventListenerFunction) -> EventListenerFunction:
         setattr(func, "_listener_meta", event)
 
         return func
