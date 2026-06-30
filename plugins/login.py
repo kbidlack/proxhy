b19e9310828657cd90dd2b60f5b321a51fbdbd7f
Kavi Bidlack <ksbidlack@gmail.com>
2026-06-30 14:50:14 -0700

    don't freeze on failed player count fetch

--- plugins/login.py
+++ plugins/login.py
@@ -474,17 +474,22 @@ class LoginPlugin:
 
     @listen_client(0x00, State.STATUS, blocking=True)
     async def packet_status_request(self: ProxhyPlugin, _):
-        async with httpx.AsyncClient() as client:
-            response = await client.get("https://compass.proxhy.com/player_count")
+        # prevent duplication in err blocks
+        motd = "§7Couldn't get Proxhy player count!"
 
         try:
+            async with httpx.AsyncClient() as client:
+                response = await client.get("https://compass.proxhy.com/player_count")
+
             int(response.text)
-            motd = f"§e§l{response.text}§r§7 player{'' if response.text == '1' else 's'} currently online."
+        except httpx.HTTPError as e:
+            self.logger.warning(f"failed to fetch proxhy player count: {e}")
         except ValueError:
             self.logger.warning(
-                "Got non-integer value from compass.proxhy.com/player_count"
+                "got non-integer value from compass.proxhy.com/player_count"
             )
-            motd = "§7Couldn't get Proxhy player count! uwu"
+        else:
+            motd = f"§e§l{response.text}§r§7 player{'' if response.text == '1' else 's'} currently online."
 
         self.server_list_ping["description"] = {"text": motd}
         self.downstream.send_packet(
